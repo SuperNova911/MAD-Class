627045dbeed04805eb28817a848ffb7cd4db8396
SuperNova911 <suwhan77@naver.com>
2019-04-18 01:43:14 +0000

    4/18 Create PushSwitch driver, Fix lab2 Makefile

---
+++ lab3_push_switch/test_push_switch.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <signal.h>
+
+#define MAX_BUTTON 9
+
+unsigned char Quit = 0;
+
+void user_signal1(int sig)
+{
+				Quit = 1;
+}
+
+int main(void)
+{
+				int index;
+				int fd;
+				int bufferSize;
+				unsigned char pushSwitchBuffer[MAX_BUTTON];
+
+				fd = open("/dev/csemad_push_switch", O_RDWR);
+
+				if (fd < 0)
+				{
+								printf("Failed to open PushSwitch driver, result: '%d'\n", fd);
+								return -1;
+				}
+
+				(void)signal(SIGINT, user_signal1);
+
+				bufferSize = sizeof(pushSwitchBuffer);
+				printf("Press <ctrl+c> to quit.\n");
+
+				while (!Quit)
+				{
+								usleep(400000);
+
+								read(fd, pushSwitchBuffer, bufferSize);
+								for (index = 0; index < MAX_BUTTON; index++)
+								{
+												printf("'%d:%d' ", index + 1, pushSwitchBuffer[index]);
+								}
+								printf("\n");
+				}
+				close(fd);
+
+				return 0;
+}
