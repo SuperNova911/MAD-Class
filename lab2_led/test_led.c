06eed16e862d533d84d5d90383fcbb90a000804b
SuperNova911 <suwhan77@naver.com>
2019-04-23 02:26:51 +0000

    Fix indent error

--- lab2_led/test_led.c
+++ lab2_led/test_led.c
@@ -5,31 +5,31 @@
 
 int main(int argc, char * argv[])
 {
-	int fd;
-	unsigned char input;
+    int fd;
+    unsigned char input;
     unsigned char currentLED;
 
     if (argc < 2 || atoi(argv[1]) < 0 || atoi(argv[1]) > 255)
-	{
-		printf("0~255 사이의 값을 입력하세요.\n");
-		return -1;
-	}
+    {
+        printf("0~255 사이의 값을 입력하세요.\n");
+        return -1;
+    }
 
-	input = (unsigned char)atoi(argv[1]);
+    input = (unsigned char)atoi(argv[1]);
 
-	fd = open("/dev/csemad_led", O_RDWR);
+    fd = open("/dev/csemad_led", O_RDWR);
 
-	if (fd < 0)
-	{
-		printf("Failed to open LED Driver, result: '%d'\n", fd);
-		return -1;
-	}
+    if (fd < 0)
+    {
+        printf("Failed to open LED Driver, result: '%d'\n", fd);
+        return -1;
+    }
 
-	write(fd, &input, 1);
-	read(fd, &currentLED, 1);
+    write(fd, &input, 1);
+    read(fd, &currentLED, 1);
 
-	printf("Current LED Value: '%d'\n", currentLED);
+    printf("Current LED Value: '%d'\n", currentLED);
 
-	close(fd);
+    close(fd);
     return 0;
 }
